67e465e5f5968038e89fb9015f4b5a56e8f2c64a
le anh vu <username>
2021-06-11 21:44:48 +0900

    add hashtag name

--- config/hashtag.php
+++ config/hashtag.php
@@ -1,9 +1,18 @@
 <?php
 
 return [
-  1 => 'IT',
-  2 => 'Sports',
-
+  1 => 'Tự nhiên',
+  2 => 'Động vật',
+  3 => 'Thực vật',
+  4 => 'Con người',
+  5 => 'Đồ vật xung quanh',
+  6 => 'Cuộc sống thường ngày',
+  7 => 'Nghệ thuật',
+  8 => 'Công việc',
+  9 => 'Học tập',
+  10 => 'Cảm xúc & Cảm giác',
+  11 => 'Thời trang',
+  12 => 'Công nghệ',
 ];
 
 // cach goi danh dach hashtag:
