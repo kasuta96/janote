de1e2f39910da6c09835d98e3e862b2b77a65dae
kasuta96 <kasuta96@gmail.com>
2021-06-12 21:42:03 +0900

    add hashtag multi language

--- config/hashtag.php
+++ config/hashtag.php
@@ -1,19 +1,49 @@
 <?php
 
 return [
-  1 => 'Tự nhiên',
-  2 => 'Động vật',
-  3 => 'Thực vật',
-  4 => 'Con người',
-  5 => 'Đồ vật xung quanh',
-  6 => 'Cuộc sống thường ngày',
-  7 => 'Nghệ thuật',
-  8 => 'Công việc',
-  9 => 'Học tập',
-  10 => 'Cảm xúc & Cảm giác',
-  11 => 'Thời trang',
-  12 => 'Công nghệ',
+  'vi' => [
+    1 => 'Tự nhiên',
+    2 => 'Động vật',
+    3 => 'Thực vật',
+    4 => 'Con người',
+    5 => 'Đồ vật xung quanh',
+    6 => 'Cuộc sống thường ngày',
+    7 => 'Nghệ thuật',
+    8 => 'Công việc',
+    9 => 'Học tập',
+    10 => 'Cảm xúc & Cảm giác',
+    11 => 'Thời trang',
+    12 => 'Công nghệ',
+  ],
+  'en' => [
+    1 => 'Natural',
+    2 => 'Animal',
+    3 => 'Plants',
+    4 => 'Human',
+    5 => 'Objects around',
+    6 => 'Daily life',
+    7 => 'Art',
+    8 => 'Work',
+    9 => 'Learning',
+    10 => 'Emotions & Feelings',
+    11 => 'Fashion',
+    12 => 'Technology', 
+  ],
+  'ja' => [
+    1 => 'ナチュラル',
+    2 => '動物',
+    3 => '植物',
+    4 => '人間',
+    5 => '周囲のオブジェクト',
+    6 => '日常生活',
+    7 => 'アート',
+    8 => '作業',
+    9 => '学習',
+    10 => '感情と感情',
+    11 => 'ファッション',
+    12 => 'テクノロジー',
+  ]
 ];
 
 // cach goi danh dach hashtag:
-// Config::get('hashtag') as $tags => $tag
+// Config::get('hashtag')[App::getLocale()] as $tags => $tag
